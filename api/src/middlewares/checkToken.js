8e7042d6b99e9b314659d1b3db024280e615ac0f
NicoRiri <nicolate.bernardet@gmail.com>
2024-04-02 14:47:34 +0200

    middleware vrmt okay

--- api/src/middlewares/checkToken.js
+++ api/src/middlewares/checkToken.js
@@ -1,16 +1,22 @@
 import {decode, verify} from "../services/JWTService.js";
 
 export default (req, res, next) => {
-    const auth = req.get("Authorization")
-    const token = auth.split(" ")[1]
-    console.log(1)
-    const ver = verify(token)
-    if (ver) {
-        const decoded = decode(token)
-        req.id = decoded.id
-        next()
-    } else {
-        res.status(401)
-        res.json({error: "Token invalide"})
+    try{
+        const auth = req.get("Authorization")
+        const token = auth.split(" ")[1]
+        console.log(1)
+        const ver = verify(token)
+        if (ver) {
+            const decoded = decode(token)
+            req.id = decoded.id
+            next()
+        } else {
+            res.status(401)
+            res.json({error: "Token invalide"})
+        }
+    } catch (e) {
+        res.status(500)
+        res.json({error:  "Token manquant"})
     }
+
 }
