a868094f3eb9d7c5f3ef5a6c2a173f6e821912bc
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-02 15:51:55 +0200

    GetSalles

--- api/src/routes/apiRouter.js
+++ api/src/routes/apiRouter.js
@@ -4,7 +4,9 @@ import {PostFavoris} from "../actions/PostFavoris.js";
 import {PostInscription} from "../actions/PostInscription.js";
 import {PostConnexion} from "../actions/PostConnexion.js";
 import {DeleteFavoris} from "../actions/DeleteFavoris.js";
+import {GetSalles} from "../actions/GetSalles.js";
 import checkToken from "../middlewares/checkToken.js";
+import { get } from "http";
 
 const router = express.Router()
 
@@ -15,5 +17,5 @@ router.post("/favoris", checkToken, (req, res) => PostFavoris(req, res))
 router.delete("/favoris", checkToken,(req, res) => DeleteFavoris(req, res))
 router.post("/inscription", (req, res) => PostInscription(req, res))
 router.post("/connexion", (req, res) => PostConnexion(req, res))
-
+router.get("/salles",(res,req)=>GetSalles(res,req))
 export default router
