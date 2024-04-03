fedcc2e4ab9c5949e9adbb0c72d6d1c2847e950e
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-03 10:25:28 +0200

    installation tailwind + test dans app.vue

--- web/app.vue
+++ web/app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nav class="bg-gray-800">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-16">
+          <div class="flex items-center px-2 lg:px-0">
+          
+            <div class="hidden lg:block lg:ml-6">
+              <div class="flex space-x-4">
+                <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
