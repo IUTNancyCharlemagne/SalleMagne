3d74ccd102f3b48936e6992d6f0c321efc91801f
NicoRiri <nicolate.bernardet@gmail.com>
2024-04-03 15:17:16 +0200

    fix: vérification strict

--- api/src/actions/PostInscription.js
+++ api/src/actions/PostInscription.js
@@ -3,18 +3,55 @@ import {hash, genSalt, compare} from "bcrypt";
 
 export async function PostInscription(req, res) {
     const auth = req.get("Authorization")
-    const b64 = auth.split(" ")[1]
-    const text = atob(b64)
+    if (auth === undefined) {
+        res.status(401)
+        res.json({status: 1, error: "Authentification requise"})
+        return
+    }
+    if (!auth.startsWith("Basic ")) {
+        res.status(401)
+        res.json({status: 1, error: "Authentification mal formée"})
+        return
+    }
+    let text = ""
+    try {
+        const b64 = auth.split(" ")[1]
+        text = atob(b64)
+    } catch (e) {
+        res.status(401)
+        res.json({status: 1, error: "Encodage mal fait"})
+        return
+    }
     const email = text.split(":")[0]
+    if (email === "") {
+        res.status(401)
+        res.json({status: 1, error: "Email requis"})
+        return
+    }
     const password = text.split(":")[1]
+    if (password === "") {
+        res.status(401)
+        res.json({status: 1, error: "Mot de passe requis"})
+        return
+    }
     const nom = req.body.nom
+    if (nom === undefined || nom === "") {
+        res.status(401)
+        res.json({status: 1, error: "Nom requis"})
+        return
+    }
     const prenom = req.body.prenom
+    if (prenom === undefined || prenom === "") {
+        res.status(401)
+        res.json({status: 1, error: "Prénom requis"})
+        return
+    }
 
     const nbRow = await db("Etudiant").count("* as nb").where("email", email)
 
-    if(nbRow[0].nb > 0){
+    if (nbRow[0].nb > 0) {
         res.status(401)
-        res.json({error: "Email déjà utilisé"})
+        res.json({status: 1, error: "Email déjà utilisé"})
         return
     }
 
@@ -25,6 +62,6 @@ export async function PostInscription(req, res) {
         res.status(201)
         res.json({status: 0, message: "Inscription Okay"})
     } catch (e) {
-        res.json({error: e})
+        res.json({status: 2, error: e})
     }
 }
