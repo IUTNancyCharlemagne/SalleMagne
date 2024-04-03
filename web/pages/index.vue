35f205d432893a5feca1ba332c48af4ffc62f27a
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-03 15:10:53 +0200

    index

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-    <div>
-        Hello world
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 class="text-4xl font-bold mb-4">Bienvenue sur Salle'MAgne</h1>
+      <div class="flex flex-col items-center justify-center mb-4">
+        <input type="text" placeholder="Entrez le numéro de salle" class="mb-2 px-4 py-2 border rounded w-full">
+        <button class="px-4 py-2 bg-blue-500 text-white rounded">Prenez une photo de la plaque de la salle</button>
+      </div>
     </div>
-</template>
-
-<script>
-</script>
+  </template>
+  
+  <script>
+  export default {
+    
+  };
+  </script>
