e676fd8281e23970438765abb7414bf50da5d3a2
NicoRiri <nicolate.bernardet@gmail.com>
2024-04-03 13:54:13 +0200

    feat: ajout des cors

--- api/src/app.js
+++ api/src/app.js
@@ -6,6 +6,7 @@ import catchAllError from "./middlewares/catchAllError.js";
 import {startCron} from "./auto/scheduler.js";
 import {readFileSync} from "fs";
 import * as https from "https";
+import cors from "cors";
 
 const privateKey = readFileSync('./certs/privkey.key', 'utf8');
 const certificate = readFileSync('./certs/cert.cer', 'utf8');
@@ -19,7 +20,7 @@ startCron()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use(express.static('public'))
 app.get("/", (req, res) => res.send("Hello depuis la racine"))
 app.use("/api", apiRouter)
