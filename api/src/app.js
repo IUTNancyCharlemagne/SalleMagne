6be92e03831d84697ab8961966783477dfff0088
NicoRiri <nicolate.bernardet@gmail.com>
2024-04-03 11:39:45 +0200

    feat: API version prod en https et http

--- api/src/app.js
+++ api/src/app.js
@@ -4,9 +4,16 @@ import apiRouter from "./routes/apiRouter.js";
 import catch404errors from "./middlewares/catch404errors.js";
 import catchAllError from "./middlewares/catchAllError.js";
 import {startCron} from "./auto/scheduler.js";
+import {readFileSync} from "fs";
+import * as https from "https";
 
+const privateKey = readFileSync('./certs/privkey.key', 'utf8');
+const certificate = readFileSync('./certs/cert.cer', 'utf8');
+
+const credentials = {key: privateKey, cert: certificate}
 const app = express();
 const httpServer = createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
 startCron()
 
@@ -19,4 +26,4 @@ app.use("/api", apiRouter)
 
 app.use(catch404errors)
 app.use(catchAllError)
-export default httpServer;
+export {httpServer, httpsServer}
--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,9 @@
-import httpServer from "./app.js";
+import {httpServer, httpsServer} from "./app.js";
 
 httpServer.listen(process.env.PORT, () => {
-    console.log(`🦉 Serveur prêt sur le port ${process.env.PORT}`);
+    console.log(`🦉 Serveur HTTP prêt sur le port ${process.env.PORT}`);
+})
+
+httpsServer.listen(process.env.PORTHTTPS, () => {
+    console.log(`🦉 Serveur HTTPS prêt sur le port ${process.env.PORTHTTPS}`);
 })
