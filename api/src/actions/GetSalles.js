dbc3b62e569bdcb2d35e1de96be262d7ce65a7ee
Jules <jules.holder.54@gmail.com>
2024-04-03 10:22:23 +0200

    GetSalles.js

--- api/src/actions/GetSalles.js
+++ api/src/actions/GetSalles.js
@@ -2,26 +2,40 @@ import fs from 'fs';
 import util from 'util';
 import ICAL from 'ical.js';
 
-const readFile = util.promisify(fs.readFile);
 
+//Récupère les salles de l'agenda et les filtre
 export async function GetSalles(req, res) {
-  try {
-    const icsData = await readFile('./public/ADECAL.ics', 'utf8');
-    const jcalData = ICAL.parse(icsData);
-    const comp = new ICAL.Component(jcalData);
-    const vevents = comp.getAllSubcomponents('vevent');
-    const data = vevents.map(vevent => {
-      const event = new ICAL.Event(vevent);
-      return {
-        location: event.location,
-        summary: event.summary,
-        startDate: event.startDate.toString(),
-        endDate: event.endDate.toString(),
-        description: event.description
-      };
-    });
-    res.json(data);
-  } catch (error) {
-    res.status(500).send(error.toString());
-  }
+    try {
+        //récupère les données du fichier ics
+        const readFile = util.promisify(fs.readFile);
+        const icsData = await readFile('./public/ADECAL.ics', 'utf8');
+        const jcalData = ICAL.parse(icsData);
+        const comp = new ICAL.Component(jcalData);
+
+        //récupère les évènements et les ajoute dans un tableau
+        const vevents = comp.getAllSubcomponents('vevent');
+        let data = vevents.map(vevent => {
+            const event = new ICAL.Event(vevent);
+            //vérifie si l'évènement est dans la semaine à venir et sélectionne la salle, le résumé, la date de début et la date de fin
+            if ((event.startDate.toJSDate() > new Date()) &&
+                (event.startDate.toJSDate() < new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000))) {
+                return {
+                    location: event.location,
+                    summary: event.summary,
+                    startDate: event.startDate.toString(),
+                    endDate: event.endDate.toString(),
+                };
+            }
+        });
+        //filtre les évènements vides
+        data = data.filter(event => event !== undefined)
+        //si un id est passé en paramètre, filtre les évènements par salle
+        if (req.query.id) data = data.filter(event => {
+            return event.location.endsWith(req.query.id);
+        });
+        //renvoie les données
+        res.json(data);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
 }
