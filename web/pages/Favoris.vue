6d585d6a713aaf79e7e20be66ae7813ebf3d97cf
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-04 12:31:44 +0200

    page favoris

---
+++ web/pages/Favoris.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="p-6 bg-gray-100 min-h-screen">
+        <h1 class="text-4xl font-bold mb-4">Favoris</h1>
+        <h2 class="text-2xl mb-6">Vos salles favorites :</h2>
+        <ul>
+            <li v-for="favori in favoris" :key="favori.salle" class="mb-2">
+                <NuxtLink :to="`/salle/${favori.salle}`" class="inline-block px-6 py-2 text-lg text-white bg-blue-500 rounded hover:bg-blue-600">
+                    {{ favori.salle }}
+                </NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { API_FAVORIS } from '~/utils/api_const';
+
+export default {
+  data() {
+    return {
+      favoris: [],
+    };
+  },
+  created() {
+    const api = axios.create({
+      baseURL: API_FAVORIS,
+      headers: {
+        'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIsImlhdCI6MTcxMjIyNDQ4NiwiZXhwIjoxNzEyMjI4MDg2fQ.Svfr99fIqZCQBOJA0UB6ydVMJfjKfDtzfG6xKAqx0uI`
+      }
+    });
+
+    api.get('/')
+      .then(response => {
+        this.favoris = response.data.data; // Modifiez cette ligne
+        console.log(this.favoris);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+};
+</script>
--- web/utils/api_const.js
+++ web/utils/api_const.js
@@ -1,3 +1,5 @@
 export const API_BASE = "https://docketu.iutnc.univ-lorraine.fr:32499/api"
 
 export const API_SALLE = API_BASE + "/salles"
+
+export const API_FAVORIS = API_BASE + "/favoris"
