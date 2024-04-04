f0918f9b935e7073d8ade6a3138a8af4b281a568
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-04 13:52:39 +0200

    truc

--- web/app.vue
+++ web/app.vue
@@ -1,24 +1,47 @@
 <template>
-  <div>
-    <nav class="bg-gray-800">
-      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div class="relative flex items-center justify-between h-16">
-          <div class="flex items-center px-2 lg:px-0">
-          
-            <div class="hidden lg:block lg:ml-6">
-              <div class="flex space-x-4">
-                <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                <NuxtLink to="/geoloc">
-                  <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Où est l'iut charlemagne ?</a>
-                </NuxtLink>
-                
-              </div>
-            </div>
-          </div>
+  <div class="flex flex-col justify-center items-center min-h-screen bg-stone-50">
+    <div class="relative flex items-center justify-between h-16 shadow rounded p-4 w-full">
+      <div class="flex space-x-6 items-center">
+        <NuxtLink to="/"
+          class="flex items-center space-x-4 text-blue-900 hover:text-blue-600 transition-colors duration-200">
+          <img src="./assets/logo_iut.png" alt="Iut Charlemagne" class="h-14 w-14">
+          <h1 class="text-2xl font-bold">Salle'Magne</h1>
+        </NuxtLink>
+        <NuxtLink to="/geoloc">
+          <a href="#"
+            class="bg-blue-900 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-blue-700 transition-colors duration-200">Où
+            est l'iut charlemagne ?</a>
+        </NuxtLink>
+        <NuxtLink to="/salles">
+          <a href="#"
+            class="bg-blue-900 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-blue-700 transition-colors duration-200">
+            Salles</a>
+        </NuxtLink>
+        <div class="relative">
+          <input type="text" class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
+            placeholder="Recherche...">
+          <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
+            <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966">
+              <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23 s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.881,2.162,0.881s1.59-0.288,2.162-0.881 C56.231,54.786,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+          </button>
         </div>
       </div>
-    </nav>
+      <div class="flex space-x-6 items-center">
+        <NuxtLink to="/Connexion">
+          <a href="#"
+            class="bg-blue-900 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-blue-700 transition-colors duration-200">
+            Connexion</a>
+        </NuxtLink>
+        <NuxtLink to="/Connexion">
+          <a href="#"
+            class="bg-blue-900 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-blue-700 transition-colors duration-200">
+            Inscription</a>
+        </NuxtLink>
+        
+      </div>
+    </div>
     <NuxtPage />
   </div>
 </template>
--- web/pages/geoloc.vue
+++ web/pages/geoloc.vue
@@ -1,40 +1,43 @@
 <template>
-    <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <button class="mb-4 px-4 py-2 bg-blue-500 text-white rounded" @click="getLocation">Obtenir votre position</button>
+  <div class="flex p-4 ">
+    <div class="w-1/2 p-4 bg-white rounded shadow">
+      <h1 class="text-2xl font-bold mb-4 text-blue-600">Où est l'Iut Charlemagne ?</h1>
+      <img src="../assets/logo_iut.png" alt="Iut Charlemagne" class="mb-4 h-16 w-16">
+      <p class="mb-4 text-lg text-gray-700">Iut Charlemagne : 2Ter Bd Charlemagne, 54000 Nancy</p>
+      <h2 class="text-xl font-bold mb-4 text-blue-600">Votre Position:</h2>
+      <button class="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" @click="getLocation">Obtenir</button>
       <div v-if="location" class="mb-4 text-lg">
         Latitude: <span class="font-bold">{{ location.latitude }}</span>
         Longitude: <span class="font-bold">{{ location.longitude }}</span>
       </div>
-      <div class="mb-4 h-64 w-64">
-        <LMap
-          ref="map"
-          :zoom="zoom"
-          :center="location ? [location.latitude, location.longitude] : [48.68301483239705, 6.16099446607644]"
-          class="rounded shadow"
-        >
-          <LTileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-            layer-type="base"
-            name="OpenStreetMap"
-          />
-          <l-marker v-if="location" :lat-lng="[location.latitude, location.longitude]"></l-marker>
-          <l-marker :lat-lng="[48.68301483239705, 6.16099446607644]">
-            <l-icon
-          :icon-size="dynamicSize"
-          :icon-anchor="dynamicAnchor">
-          <img src="../assets/logo_iut.png" alt="Iut Charlemagne" class="mb-4 h-8 w-8  shadow">
-        
-          </l-icon>     
-    </l-marker> <!-- Iut Charlemagne -->
-          <!-- <l-marker :lat-lng="[48.68301483239705, 6.16099446607644]"></l-marker> -->
-        </LMap>
-      </div>
-      <!-- <img src="../assets/logo_iut.png" alt="Iut Charlemagne" class="mb-4 h-16 w-16 rounded-full shadow"> -->
-      <p class="mb-4 text-lg text-gray-700">Iut Charlemagne : 2Ter Bd Charlemagne, 54000 Nancy</p>
-      <a :href="googleMapsUrl" target="_blank" class="px-4 py-2 bg-green-500 text-white rounded">Ouvrir dans Google Maps</a>
     </div>
-  </template>
+    <div class="w-1/2 p-4 bg-white rounded shadow">
+      <h2 class="text-xl font-bold mb-4 text-blue-600">Carte:</h2>
+      <LMap
+        ref="map"
+        :zoom="zoom"
+        :center="location ? [location.latitude, location.longitude] : [48.68301483239705, 6.16099446607644]"
+        class="rounded shadow h-full w-full"
+      >
+        <LTileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+          layer-type="base"
+          name="OpenStreetMap"
+        />
+        <l-marker v-if="location" :lat-lng="[location.latitude, location.longitude]"></l-marker>
+        <l-marker :lat-lng="[48.68301483239705, 6.16099446607644]">
+          <l-icon
+            :icon-size="dynamicSize"
+            :icon-anchor="dynamicAnchor">
+            <img src="../assets/logo_iut.png" alt="Iut Charlemagne" class="mb-4 h-8 w-8  shadow">
+          </l-icon>     
+        </l-marker> <!-- Iut Charlemagne -->
+      </LMap>
+      <a :href="googleMapsUrl" target="_blank" class="block mt-4 text-blue-500 hover:text-blue-600">Ouvrir dans Google Maps</a>
+    </div>
+  </div>
+</template>
   <script>
 
   import { ref } from 'vue';
