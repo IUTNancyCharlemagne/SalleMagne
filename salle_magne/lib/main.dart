111c49459419eecf9704be07caa2fd6fe18f2e14
Hergott <emilien.herg@gmail.com>
2024-04-05 13:04:43 +0200

    calendrier fonctionnel

--- salle_magne/lib/main.dart
+++ salle_magne/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'package:salle_magne/widget/app_navigation.dart';
+import 'dart:io';
+
+ class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(AppNavigation());
 }
