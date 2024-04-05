4e3950f8472f12dc8fb9906aa12dbc82a0d66fed
NicoRiri <nicolate.bernardet@gmail.com>
2024-04-05 21:19:03 +0200

    refac: passage en http pas S

--- api/src/app.js
+++ api/src/app.js
@@ -4,17 +4,10 @@ import apiRouter from "./routes/apiRouter.js";
 import catch404errors from "./middlewares/catch404errors.js";
 import catchAllError from "./middlewares/catchAllError.js";
 import {startCron} from "./auto/scheduler.js";
-import {readFileSync} from "fs";
-import * as https from "https";
 import cors from "cors";
 
-const privateKey = readFileSync('./certs/privkey.key', 'utf8');
-const certificate = readFileSync('./certs/cert.cer', 'utf8');
-
-const credentials = {key: privateKey, cert: certificate}
 const app = express();
 const httpServer = createServer(app);
-const httpsServer = https.createServer(credentials, app);
 
 startCron()
 
@@ -27,4 +20,4 @@ app.use("/api", apiRouter)
 
 app.use(catch404errors)
 app.use(catchAllError)
-export {httpServer, httpsServer}
+export {httpServer}
--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,5 @@
-import {httpServer, httpsServer} from "./app.js";
+import {httpServer} from "./app.js";
 
 httpServer.listen(process.env.PORT, () => {
     console.log(`🦉 Serveur HTTP prêt sur le port ${process.env.PORT}`);
-})
-
-httpsServer.listen(process.env.PORTHTTPS, () => {
-    console.log(`🦉 Serveur HTTPS prêt sur le port ${process.env.PORTHTTPS}`);
 })
