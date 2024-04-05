88a6d7ab7de359ac6c08513f910df1b9dc9a2c90
Jules <jules.holder.54@gmail.com>
2024-04-05 11:11:26 +0200

    feat: ajout classes tailwind pour le style des number input

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,16 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [function ({addUtilities}) {
+        const newUtilities = {
+            '.number-input::-webkit-inner-spin-button': {
+                '-webkit-appearance': 'none',
+                'margin': '0',
+            },
+            '.number-input::-webkit-outer-spin-button': {
+                '-webkit-appearance': 'none',
+                'margin': '0',
+            },
+            '.number-input': {
+                '-moz-appearance': 'textfield',
+            },
+        }
+        addUtilities(newUtilities, ['responsive', 'hover'])
+    }],
 }
 
