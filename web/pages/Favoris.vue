d21f8f3db10dcce19d755198e7af47283a1e32ca
ClemGrom <clement.gromangin5@etu.univ-lorraine.fr>
2024-04-05 11:16:25 +0200

    bouton tout selectionner  et css

--- web/pages/Favoris.vue
+++ web/pages/Favoris.vue
@@ -2,19 +2,23 @@
   <div class="p-6 bg-gray-100 min-h-screen">
     <h1 class="text-4xl font-bold mb-4">Favoris</h1>
     <h2 class="text-2xl mb-6">Trouvez ici vos salles favorites :</h2>
+    <button @click="selectAll"  class=" px-1 py-1 text-lg text-white bg-gray-700 rounded hover:bg-gray-600">
+    Tout sélectionner
+  </button>
+    <button @click="deleteSelectedFavoris"
+      class=" mx-1 px-1 py-1 text-lg text-white bg-red-700 rounded hover:bg-red-600">
+      Supprimer
+    </button>
     <ul>
       <li v-for="favori in favoris" :key="favori.salle" class="mb-2">
-        <input type="checkbox" :value="favori" v-model="selectedFavoris">
+        <input type="checkbox" :value="favori" v-model="selectedFavoris" class="mr-2">
         <NuxtLink :to="`/salle/${favori.salle}`"
           class="inline-block px-6 py-2 text-lg text-white bg-blue-700 rounded hover:bg-blue-600">
           Salle {{ favori.salle }}
         </NuxtLink>
       </li>
     </ul>
-    <button @click="deleteSelectedFavoris"
-      class="mt-4 px-6 py-2 text-lg text-white bg-red-700 rounded hover:bg-red-600">
-      Supprimer les favoris sélectionnés
-    </button>
+    
   </div>
 </template>
 <script>
@@ -38,6 +42,13 @@ export default {
     this.loadFavoris();
   },
   methods: {
+    selectAll() {
+  if (this.selectedFavoris.length === this.favoris.length) {
+    this.selectedFavoris = [];
+  } else {
+    this.selectedFavoris = this.favoris.slice();
+  }
+},
     loadFavoris() {
       const api = axios.create({
         baseURL: API_FAVORIS,
