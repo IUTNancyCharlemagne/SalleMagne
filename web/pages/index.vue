56d6c11b7cdb82eb19d7dc686367450466cb0a3d
Jules <sonij590833@gmail.com>
2024-04-05 13:47:59 +0200

    fix: petite correction et detail

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center  h-[92vh] justify-center  bg-gray-100 p-4">
-    <h1 class="text-4xl font-bold mb-4">Bienvenue sur Salle'MAgne</h1>
+    <h1 class="text-4xl font-bold mb-4">Bienvenue sur Salle'Magne</h1>
     <div class="flex flex-col items-center justify-center mb-4">
       <input type="text" v-model="salle" placeholder="Entrez le numéro de salle" class="mb-2 px-4 py-2 border rounded w-full">
       <div class="flex items-center space-x-4">
